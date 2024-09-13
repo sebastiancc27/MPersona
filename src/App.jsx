7115e5a21fdd6b1325c061172642253ae99c15f1
sebastiancc27 <sebasccastizo27@gmail.com>
2024-09-13 02:20:07 -0600

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    // Cargar el archivo de música desde la carpeta public
+    const audio = new Audio('/music.mp3');
+
+    const playAudio = async () => {
+      try {
+        await audio.play(); // Intenta reproducir la música
+      } catch (err) {
+        console.log('La reproducción automática fue bloqueada:', err);
+      }
+    };
+
+    playAudio(); // Llama a la función para reproducir el audio
+
+    // Limpiar al desmontar el componente
+    return () => {
+      audio.pause(); // Pausar la música
+      audio.currentTime = 0; // Reiniciar el tiempo del audio
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container_postif_container">
+        <div className="posfif_container">
+          <div className="postif">
+            <p className="postif_text">
+              Love Each Other <br />            
+              Even When We
+              <br />
+              Hate Each Other
+              <br />
+              No Running
+              <br />
+              Take Care When We Are Old,
+              <br />
+              Senile and Smelly <br />
+              And it´s forever
+            </p>
+            
+            <p className="postif_person">Derek Shepherd</p>
+            <p className="postif_person">Meredith Grey</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
